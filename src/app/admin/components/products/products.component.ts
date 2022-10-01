2517dc5dc33993e3845c1d1440c52c98009a4f66
Mert <zincir_890@hotmail.com>
2022-10-01 22:18:41 +0300

    Created Custom HttpClient Service

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { Product } from 'src/app/contracts/product';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
   selector: 'app-products',
@@ -9,12 +11,43 @@ import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 })
 export class ProductsComponent extends BaseComponent implements OnInit {
 
-  constructor(spinner: NgxSpinnerService) {
+  constructor(spinner: NgxSpinnerService, private httpClientService: HttpClientService) {
     super(spinner);
   }
 
   ngOnInit(): void {
     this.showSpinner(SpinnerType.BallAtom);
+
+    this.httpClientService.get<Product[]>({
+      controller: "products"
+    }).subscribe(data => console.log(data));
+
+    // this.httpClientService.post({
+    //   controller: "products"
+    // }, {
+    //   name: "Kalem",
+    //   stock: 100,
+    //   price: 15
+    // }).subscribe();
+
+    // this.httpClientService.put({
+    //   controller: "products",
+    // }, {
+    //   id: "e4bd504c-2224-4ed3-a8a7-1ccd05971473",
+    //   name: "Renkli Kağıt",
+    //   stock: 1500,
+    //   price: 5.5
+    // }).subscribe()
+
+    // this.httpClientService.delete({
+    //   controller: "products"
+    // }, "00734fe6-620d-46d3-8c26-aced2662b63d")
+    // .subscribe();
+
+    // this.httpClientService.get({
+    //   baseUrl: "https://jsonplaceholder.typicode.com",
+    //   controller: "posts"
+    // }).subscribe(data=> console.log(data));
   }
 
 }
